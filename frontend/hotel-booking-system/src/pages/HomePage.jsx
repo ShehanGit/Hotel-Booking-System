d8d7a9bda227ebcfe3cc79c48025e9201527a366
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-07-09 15:52:09 +0530

    talwind css add

---
+++ frontend/hotel-booking-system/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavBar } from '../components/NavBar';
+
+import { Button } from "flowbite-react";
+
+
+function HomePage() {
+  return (
+    <div className="App">
+        <NavBar />
+        <h1>Welcome to My React App</h1>
+    </div>
+  );
+}
+
+export default HomePage;
